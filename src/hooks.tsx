0cb3653db7155dbfd5a468a527531e43e3aebada
Khanh <tran_khanh@outlook.com>
2020-12-17 18:00:13 +0700

    user canclick out side the element

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { RefObject, useEffect } from 'react';
+import { EventType } from 'types';
 
 export const useOnEscape = (handler: () => void, active = true) => {
   useEffect(() => {
@@ -31,4 +32,37 @@ export const useRepositionOnResize = (handler: () => void, active = true) => {
       window.removeEventListener('resize', listener);
     };
   }, [handler, active]);
+};
+
+export const useOnClickOutSide = (
+  ref: RefObject<HTMLElement> | RefObject<HTMLElement>[],
+  handler: () => void,
+  active = true
+) => {
+  useEffect(() => {
+    if (!active) return;
+    const listener = (event: any) => {
+      // Do nothing if clicking ref's element or descendent elements
+      const refs = Array.isArray(ref) ? ref : [ref];
+
+      let contains = false;
+      refs.forEach((r) => {
+        if (!r.current || r.current.contains(event.target)) {
+          contains = true;
+          return;
+        }
+      });
+      event.stopPropagation();
+      if (!contains) handler();
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      if (!active) return;
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler, active]);
 };
