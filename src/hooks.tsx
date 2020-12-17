d08efe8c90dad544907c2847e3887b4c6a292585
Khanh <tran_khanh@outlook.com>
2020-12-17 17:50:37 +0700

    implement useRepositionOnResize

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -1,19 +1,34 @@
+import { useEffect } from 'react';
 
-import { useLayoutEffect,RefObject, useEffect} from "react";
+export const useOnEscape = (handler: () => void, active = true) => {
+  useEffect(() => {
+    if (!active) return;
 
-export const useOnEscape = (handler: ()=> void, active =true){
-    useEffect(()=>{
-        if(!active) return;
-        
-        const listener = (event: any)=>{
-            if(event.key === 'Escape') handler();
-        }
+    const listener = (event: any) => {
+      if (event.key === 'Escape') handler();
+    };
 
-        document.addEventListener('keyup', listener)
+    document.addEventListener('keyup', listener);
 
-        return ()=>{
-            if(!active) return;
-            document.removeEventListener('keyup',listener)
-        }
-    }, [handler, active])
+    return () => {
+      if (!active) return;
+      document.removeEventListener('keyup', listener);
+    };
+  }, [handler, active]);
+};
+
+export const useRepositionOnResize = (handler: () => void, active = true) => {
+  useEffect(() => {
+    if (!active) return;
+    const listener = () => {
+      handler();
+    };
+
+    window.addEventListener('resize', listener);
+
+    return () => {
+      if (!active) return;
+      window.removeEventListener('resize', listener);
+    };
+  }, [handler, active]);
 };
